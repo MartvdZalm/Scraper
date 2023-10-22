d9896cae9699d74ec5e1a031ab97ef22e23383b8
MartvdZalm <martvanderzalm@gmail.com>
2023-10-22 16:11:57 +0200

    Scraper changes

--- src/Scraper.php
+++ src/Scraper.php
@@ -3,21 +3,24 @@ namespace KnightScraper;
 
 class Scraper
 {
-	const VERSION = '1.1';
-	
 	protected string $url;
 	protected Logger $logger;
+	protected Curl $curl;
 
 	public function __construct(string $url = '')
 	{
 		$this->url = $url;
 		$this->logger = new Logger();
+
+		if (empty($url)) {
+			$this->curl = new CurlHandle();
+		} else {
+			$this->curl = new CurlHandle($url);
+		}
 	}
 
-	protected function validUrl(): bool
+	public function start(): string
 	{
-		if (is_null($this->url)) {
-			$this->logger->error("URL is null");
-		}
+
 	}
 }
