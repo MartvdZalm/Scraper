cd132b4c658ddb9cafc6e6b2f5b078332fbd1ba3
Mart van der Zalm <martvanderzalm@gmail.com>
2023-11-03 22:56:11 +0100

    Logger

--- src/Logger.php
+++ src/Logger.php
@@ -4,23 +4,23 @@ namespace KnightScraper;
 
 class Logger
 {
-	public function __construct()
-	{
+    public function __construct()
+    {
 
-	}
+    }
 
-	public function error(string $message): void
-	{
-		echo('Error: '. $message .'\n');
-	}
+    public function error(string $message): void
+    {
+        echo("Error: " . $message . "\n");
+    }
 
-	public function warning(string $message): void
-	{
-		echo('Warning: '. $message .'\n');
-	}
+    public function warning(string $message): void
+    {
+        echo("Warning: " . $message . "\n");
+    }
 
-	public function info(string $message): void
-	{
-		echo('Info: '. $message .'\n');
-	}
+    public function info(string $message): void
+    {
+        echo("Info: " . $message . "\n");
+    }
 }
