53cae0faa77152e62f457f4158ecf007fa3c4a48
Mart van der Zalm <martvanderzalm@gmail.com>
2023-11-14 00:17:05 +0100

    Logger

--- src/Logger/Logger.php
+++ src/Logger/Logger.php
@@ -4,28 +4,28 @@ namespace KnightScraper\Logger;
 
 class Logger
 {
-    public function __construct()
-    {
+	public function __construct()
+	{
 
-    }
+	}
 
-    private function getDate(): string
-    {
-        return '['.date('H:i:s').']';
-    }
+	private function getDate(): string
+	{
+		return '['.date('H:i:s').']';
+	}
 
-    public function error(string $message): void
-    {
-        echo($this->getDate()." Error: " . $message . "\n");
-    }
+	public function error(string $message): void
+	{
+		echo($this->getDate().' Error: ' . $message . "\n");
+	}
 
-    public function warning(string $message): void
-    {
-        echo($this->getDate()." Warning: " . $message . "\n");
-    }
+	public function warning(string $message): void
+	{
+		echo($this->getDate().' Warning: ' . $message . "\n");
+	}
 
-    public function info(string $message): void
-    {
-        echo($this->getDate()." Info: " . $message . "\n");
-    }
+	public function info(string $message): void
+	{
+		echo($this->getDate().' Info: ' . $message . "\n");
+	}
 }
