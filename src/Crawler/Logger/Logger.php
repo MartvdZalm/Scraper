c61fbcaf8bfb0336abc9ba3642b646763f039fbf
Mart van der Zalm <martvanderzalm@gmail.com>
2023-11-16 00:11:06 +0100

    Logger

---
+++ src/Crawler/Logger/Logger.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Scraper\Crawler\Logger;
+
+class Logger
+{
+	public function __construct()
+	{
+
+	}
+
+	private function getDate(): string
+	{
+		return '['.date('H:i:s').']';
+	}
+
+	public function error(string $message): void
+	{
+		echo($this->getDate().' Error: ' . $message . "\n");
+	}
+
+	public function warning(string $message): void
+	{
+		echo($this->getDate().' Warning: ' . $message . "\n");
+	}
+
+	public function info(string $message): void
+	{
+		echo($this->getDate().' Info: ' . $message . "\n");
+	}
+}
